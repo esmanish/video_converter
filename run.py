49c1d1ed936fbe8f96d67696509be14ad47a5239
Manish_Edassery <41136588+esmanish@users.noreply.github.com>
2021-11-14 16:17:41 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 import moviepy.editor as moviepy
-clip = moviepy.VideoFileClip("video.mov") #include your location of your video with the video extension here.
+clip = moviepy.VideoFileClip("video.mov") #include location of your video with it's extension here.
 clip.write_videofile("output.mp4") # output video name and the extension to which you want to convert your video.
